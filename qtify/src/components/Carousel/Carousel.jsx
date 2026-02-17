6974f3c3bf70615bbbb0dd0bc78c6914a4080775
Akash Manwar <akashmanwar@Akashs-MacBook-Air.local>
2026-02-18 02:41:52 +0530

    fixed errors

--- qtify/src/components/Carousel/Carousel.jsx
+++ qtify/src/components/Carousel/Carousel.jsx
@@ -24,7 +24,7 @@ function Carousel({ data, renderComponent }) {
         style={{ padding: "0px 20px" }}
         initialSlide={0}
         modules={[Navigation]}
-        slidesPerView={"auto"}
+        slidesPerView={4}
         spaceBetween={40}
         allowTouchMove
       >
